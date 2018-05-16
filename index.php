8404115d81a7088b0f37b5295eb5eb7ad0cd2a7b
Jonathan <jonbarlo@gmail.com>
2018-05-16 17:53:12 -0600

    Code Refactor: added new php file for testing WP Plugin's core functionality, removed google fonts, removed unused text file

--- index.php
+++ index.php
@@ -1,28 +1,18 @@
-<!--
-//
-//base styles and fonts
-//
--->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Magra" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Varela" rel="stylesheet">
-
 <?php
 //---------------------
 //header information
 //---------------------
 /**
 	Plugin Name: Artwork Archive Artist's Gallery Plugin
-	Plugin URI: https://github.com/artworkarchive/aa_wp_plugin
+	Plugin URI: https://github.com/artworkarchive/aa20-wp-plugin
 	Description: Artwork Archive wordpress plugin which lets the user to pull down public data, including user's public pieces as well as user's public general information
-	Version: 1.1.2
+	Version: 1.1.6
 	Author: Artwork Archive (John Feustel & Jonathan Barquero)
 	Author URI: http://www.artworkarchive.com/
     License: GPLv2 or later
 	Text Domain: artwork-archive-gallery
 	
-	Copyright 2018  Smash Balloon LLC (email : hey@smashballoon.com)
+	Copyright 2018  Artwork Archive LLC (email : john@artworkarchive.com)
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation; either version 2 of the License, or
@@ -33,7 +23,7 @@
 	GNU General Public License for more details.
 	You should have received a copy of the GNU General Public License
 	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	Foundation, Inc., Denver, CO, USA
 */
 
 //---------------------------------------------------------------
@@ -64,24 +54,4 @@ add_shortcode('artworkarchive_artist_gallery', 'get_user_public_gallery' );
 //---------------------------------------------------------------
 wp_enqueue_style( 'wp-aa-style', plugin_dir_url( __FILE__ ) . 'css/wp-aa-style.css',false,'1.1','all');
 wp_enqueue_style( 'wp-aa-modal', plugin_dir_url( __FILE__ ) . 'css/wp-aa-modal.css',false,'1.1','all');
-
-
-/*
-
-//---------------------------------------------------------------
-//Hardcode Localhost Test comment#1
-//---------------------------------------------------------------
-echo ArtworkArchiveApiHelper::generate_public_pieces_modal_popups('sophia-rouhana');
-echo ArtworkArchiveApiHelper::get_user_public_pieces_information('sophia-rouhana');
-
-*/
 ?>
-
-<!--//Hardcode Test comment#2-->
-
-<!--
-
-<link href="http://public.aeonitgroup.com/aeon/artworkarchive/aa20-wp-plugin/css/wp-aa-style.css" rel="stylesheet">
-<link href="http://public.aeonitgroup.com/aeon/artworkarchive/aa20-wp-plugin/css/wp-aa-modal.css" rel="stylesheet">
-
--->
---
+++ test.php
@@ -0,0 +1,27 @@
+<b>Artwork Archive</b>
+<i> WordPress Plugin's</i>
+<small>Test Page</small>
+<small>: php</small>
+
+<?php
+//---------------------------------------------------------------
+// Hardcode Test Showing how does this plugin works
+// Written in php
+// Doesn't requires any WordPress installation
+//---------------------------------------------------------------
+
+//---------------------------------------------------------------
+//Include Artwork Archive Core/Base Class for WordPress Plugin
+//---------------------------------------------------------------
+require_once('api_helper.php');
+
+//Artwork Archive Default Test Artist Name
+$my_artist_slug = 'sophia-rouhana';
+
+echo ArtworkArchiveApiHelper::generate_public_pieces_modal_popups($my_artist_slug);
+echo ArtworkArchiveApiHelper::get_user_public_pieces_information($my_artist_slug);
+?>
+
+<!-- Hardcode Localhost Test CSS Static Styles Include -->
+<link href="http://public.aeonitgroup.com/aeon/artworkarchive/aa20-wp-plugin/css/wp-aa-style.css" rel="stylesheet">
+<link href="http://public.aeonitgroup.com/aeon/artworkarchive/aa20-wp-plugin/css/wp-aa-modal.css" rel="stylesheet">
